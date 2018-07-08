80e43d4b30d4897c7bf82702a343182bd44feba7
Andrew Tran <abtran@gmail.com>
2018-07-08 12:42:55 -0400

    new files

---
+++ case_study/key.R
@@ -0,0 +1,5 @@
+# this key will not be functional after the IRE conference
+# Replace it with your own by signing up at this site
+# http://api.census.gov/data/key_signup.html
+
+census_key <- "33db52b83cf8c0da82b49dd713c0018f3e9912a8"
